e8747c21f3c443d675bca89b51ed835e2dc6610c
Fabio Kiatkowski <xolla@xolla.com.br>
2020-08-11 18:00:35 -0300

    Altera quantidade

--- src/data/data2.js
+++ src/data/data2.js
@@ -3,7 +3,7 @@ const items = [
     { name: "Fraldas M Pampers®", qtd: 34,},
     { name: "Fraldas G Pampers®", qtd: 55,},
     { name: "Creme Bepantol® e Lenços Umidecido", qtd: 6,},
-    { name: "Cotonets® para bêbes, Algodão e Alcool 70%", qtd: 3,},
+    { name: "Cotonets® para bêbes, Algodão e Alcool 70%", qtd: 1,},
     { name: "Sabonete Líquido e Lenço Umidecido", qtd: 10,},
     { name: "Condicionador e Óleo para bêbe", qtd: 2,},
     { name: "Shampoo e Hidrante para recém nascidos", qtd: 2,},
